f301f4816e69ae24830e77c4ef937ce25024310a
ronaldo.caetano <ronaldo.caetano@acct.global>
2020-07-05 00:08:03 -0300

    adding function to register prods with img

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import connection from '../connection'
+import axios from 'axios'
+import FormData from 'form-data'
 
 export default class UsersController {
     async index(req: Request, res: Response) {
@@ -17,12 +19,18 @@ export default class UsersController {
             query: { city },
         } = req
         if (city) {
-            connection.query(`SELECT * FROM produtos WHERE cidade = '${city}'`, (err: any, result: any) => {
-                if (err) {
-                    res.status(400).send(err)
+            connection.query(
+                `SELECT pro.id, pro.nome, pro.descricao, pro.fk_categoria, pro.prod_subcategoria, pro.preco_base, pro.preco, pro.peso, pro.comprimento, pro.altura, pro.largura, pro.fk_vendedor, pro.cidade, img.url_imagem 
+                FROM produtos pro 
+                INNER JOIN produtos_imagens img ON img.fk_produto = pro.id 
+                WHERE cidade = '${city}'`,
+                (err: any, result: any) => {
+                    if (err) {
+                        res.status(400).send(err)
+                    }
+                    res.status(200).send(result.rows)
                 }
-                res.status(200).send(result.rows)
-            })
+            )
         }
     }
 
@@ -37,33 +45,46 @@ export default class UsersController {
             height = 0,
             length = 0,
             category = 1,
-            imageUrl = 'teste',
             city,
+            imageUrl,
         } = req.body
 
         await connection.query(
             `
-            INSERT INTO public.produtos(nome, descricao, preco_base, preco, peso, comprimento, altura, largura, fk_categoria, fk_vendedor, cidade) 
-            VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
+            INSERT INTO produtos (nome, descricao, preco_base, preco, peso, comprimento, altura, largura, fk_categoria, fk_vendedor, cidade) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
             [name, description, basePrice, listPrice, weight, length, height, width, category, 2, city]
         )
 
         const getInfoFromProduct = await connection.query('SELECT * FROM produtos WHERE nome = $1', [name])
 
         if (getInfoFromProduct?.rows?.length) {
-            console.log(getInfoFromProduct)
+            JSON.parse(imageUrl).forEach(async (image: any) => {
+                const data = new FormData()
+                data.append('image', image)
 
-            const insertOnProductsImages = await connection.query(
-                `INSERT INTO produtos_imagens(fk_produto, url_imagem) VALUES ($1, $2)`,
-                [getInfoFromProduct.rows[0].id, imageUrl]
-            )
+                const config: any = {
+                    method: 'post',
+                    url: 'https://api.imgur.com/3/image',
+                    headers: {
+                        Authorization: 'Client-ID b7631a53628082f',
+                        ...data.getHeaders(),
+                    },
+                    data: data,
+                }
 
-            if (insertOnProductsImages.rowCount > 0) {
-                res.status(200).send({
-                    message: 'Registro criado com sucesso',
-                    data: getInfoFromProduct.rows,
+                await axios(config).then(async (result) => {
+                    await connection.query(`INSERT INTO produtos_imagens(fk_produto, url_imagem) VALUES ($1, $2)`, [
+                        getInfoFromProduct.rows[0].id,
+                        result.data.data.link,
+                    ])
                 })
-            }
+            })
+
+            res.status(200).send({
+                message: 'Registro criado com sucesso',
+                data: getInfoFromProduct.rows,
+            })
         }
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,8 @@ import routes from './routes'
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.urlencoded({ limit: '50mb' }))
+app.use(express.json({ limit: '50mb' }))
 app.use(routes)
 
 const port = process.env.PORT || 3333
